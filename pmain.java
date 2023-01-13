2b0013f00ff7da85d967e9dae33f4d7c58c66a8d
AmshuGM <99896640+AmshuGM@users.noreply.github.com>
2023-01-13 13:04:51 +0530

    Add files via upload

---
+++ pmain.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import cie.internals;
+import see.externals;
+
+class pmain {
+public static void main(String xx[]) {
+Scanner s=new Scanner(System.in);
+internals a1 = new internals();
+externals b1= new externals();
+}
+}
